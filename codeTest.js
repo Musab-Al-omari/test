494ede23f16e92cd7e3fae077b2e0ff1ab497aac
Musab-Al-omari <musabsomari@gmail.com>
2021-10-25 21:55:45 +0300

    first commit

---
+++ codeTest.js
@@ -0,0 +1,73 @@
+// شركة ناسي اسمها
+////////////////////////////////////////////
+// what is the abstract class ?
+// what is the oop princeple ?
+// what s redux and why we use it ?
+// what happen when there are 2 action with the same name in different reduser 
+// componnent  life cycle 
+// use effect arrgument 
+
+//  شركة ناسي اسمهاموجودة في جبل الحسين 
+
+// what is transatcion in the database ? 
+// what is the inner joint ? 
+
+// SKY LINE  company
+// THEY GIVE ME TASK ABOUT FLUTTER 
+// WHAT IS the differant between sql nosql ?
+// about firebase servises 
+
+
+
+
+
+//  sotiomTech interview qustion 
+let word ='hi my name is omar'
+
+// find the most repeted letter 
+// find the most repeted word in pharagraph 
+// hash map best answer 
+
+
+let newNumber= 550
+// find all prime number from 1 till 550
+
+
+let a=5
+let b=6
+
+// a=a+b
+// b=b-a
+// a=a-b
+// switch between these number using math , shoud be solve with math
+//(without create new variable or make the number as string) 
+
+// as input
+let khaledNumber=321
+// as output
+khaledNumbe=123
+// switch between these number using math , shoud be solve with math
+//(without create new variable or make the number as string) 
+
+
+// reverse string
+reversStrin='hello'
+
+// validate the paranthsese
+// ({{}})
+
+
+// input string1='hello word' string2='hi omar'
+// find letters which are in the both strings 
+// find the second biggest number  in the array
+
+
+
+
+
+
+
+
+
+
+
